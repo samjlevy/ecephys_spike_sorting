1bc120e3815b4735ce9e3996d65627a72d29082b
Emily Aery Jones <emily.aster.jones@gmail.com>
2023-01-18 09:26:35 -0800

    chanMapFromImro from Kilosort, but works for 2.0 probes

---
+++ ecephys_spike_sorting/createChanMapFileFromImro.m
@@ -0,0 +1,44 @@
+% main data
+directory = 'Z:\Chronic_NPX_Troubleshooting\2_probe_pilot\IMRO';
+name = 'Orville_MEC_spare';
+
+%%
+% read imro file
+imroData = readmatrix(fullfile(directory, [name, '.imro']), 'FileType','text','Delimiter',')(','OutputType','string');
+channel_list = zeros(length(imroData)-1,1);
+shank = zeros(length(imroData)-1,1);
+bank = zeros(length(imroData)-1,1);
+for i = 1:length(imroData)-1
+    c = double(split(imroData(i+1),' '));
+    if i == length(imroData)-1
+        c_end = split(imroData(i+1),' ');
+        c_end = c_end{end};
+        channel_list(i) = str2double(c_end(1:end-1)) + 1;
+    else
+        channel_list(i) = c(5) + 1;
+    end
+    shank(i) = c(2) + 1;
+    bank(i) = c(3);
+end
+kcoords = shank;
+
+%%
+% create a channel map
+pos = [32, 0, 32, 0]; % possible x-positions on shank
+Nchannels = 384; % if there is a sync channel, otherwise 384
+connected = true(Nchannels, 1);
+connected(192, 1) = false; % exclude reference and sync channel
+chanMap = [1:Nchannels]';
+chanMap0ind = chanMap - 1;
+xcoords = zeros(Nchannels,1);
+ycoords = zeros(Nchannels,1);
+for i = 1:length(channel_list)
+    ycoords(i,1) = round((channel_list(i)+bank(i)*(Nchannels-1))/2) * 15;
+    xcoords(i,1) = pos(mod(channel_list(i),4)+1);
+end
+
+fs = 30000; % sampling frequency
+
+%%
+% save data in a mat file
+save(fullfile(directory, [name, '.mat']), 'chanMap','connected', 'xcoords', 'ycoords', 'kcoords', 'chanMap0ind', 'name')
