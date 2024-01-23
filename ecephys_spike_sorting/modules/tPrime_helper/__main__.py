0f03272f7b258a7b46ae1708e6161e0ca0c9782b
Emily Aery Jones <emily.aster.jones@gmail.com>
2024-01-23 09:04:59 -0800

    Fix issue if catgt outputs empty all_fyi.txt

--- ecephys_spike_sorting/modules/tPrime_helper/__main__.py
+++ ecephys_spike_sorting/modules/tPrime_helper/__main__.py
@@ -35,7 +35,7 @@ def call_TPrime(args):
     first_gate, last_gate = SpikeGLX_utils.ParseGateStr(args['catGT_helper_params']['gate_string'])
     catGT_dest = args['directories']['extracted_data_directory']
     run_name = args['catGT_helper_params']['run_name'] + '_g' + str(first_gate)
-    run_dir_name = 'supercat_' + run_name
+    run_dir_name = 'catgt_' + run_name
     prb_dir_prefix = run_name + '_imec'
     
     run_directory = os.path.join( catGT_dest, run_dir_name ) # extracted edge files for aux data reside in run directory
@@ -45,7 +45,7 @@ def call_TPrime(args):
     
     # check for presence of an fyi file, indicating run with catgt 3.0 or later
     fyi_path = run_directory.replace('\\', '/') + '/' + run_name + '_all_fyi.txt'
-    all_fyi_exists = Path(fyi_path).is_file()
+    all_fyi_exists = (Path(fyi_path).is_file() and os.path.getsize(fyi_path)>0)
     
     
     if not all_fyi_exists:
